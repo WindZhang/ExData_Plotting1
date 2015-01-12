750287102ccca4a62c83037a346b5cc8f9b3d9d7
WindZhang <wind,zhang@139.com>
2015-01-12 10:10:18 +0800

    Home work

--- plot2.R
+++ plot2.R
@@ -1,4 +1,3 @@
-png("plot1.png")
 # Load the file to the memory
 xhousehold_power_consumption <- read.table("~/household_power_consumption.txt", header = TRUE, sep=";", quote="\"")
 # subset only two days as needed
@@ -6,12 +5,14 @@ x <- subset (xhousehold_power_consumption, Date == "2/2/2007" | Date == "1/2/200
 # convert the factor format to the csv
 write.csv(x, "~/power_2007_Feb_1_and_2.csv")
 # load the csv file into memory
-power_200702 <- read.csv("~/power_2007_Feb_1_and_2.csv")
+power <- read.csv("~/power_2007_Feb_1_and_2.csv")
 
-# plot the historam 
+png("plot2.png")
 
+#plot the data
+plot(1:length(power$Global_active_power), power$Global_active_power, type = "s", xaxt ="n", ylab = "Global Active Power(Killowatts)", xlab = "")
+#Replace the axis with "Thu", "Fri" and "Sat")
 
-
-hist(power_200702$Global_active_power, main="Global Active Power", xlab = "Global active power(Kilowatts)", col = "Red")
+axis (1, at = c(0,1440,2880) , labels = c("Thu", "Fri", "Sat"))
 
 dev.off()
